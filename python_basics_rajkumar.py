9958620f661f94472261d8d72b5145e89570261c
rajkumarpanjala1990 <rajkumarpanjala1990@gmail.com>
2026-04-03 12:17:56 +0530

    Adding new python_basics_rajkumar.py file

---
+++ python_basics_rajkumar.py
@@ -0,0 +1,10 @@
+name = "Raj Kumar"
+age = 35
+height = 5.7
+students = "Yes"
+print(name, age, height, students)
+print("Name:", name, "Age:", age, "Height:", height, "Students:", students)
+print("Age in months:", age * 12)
+print("Age in days:", age * 365)
+print("Remainder when age is divided by 7:", age % 7)
+print("Age raised to the power of 2:", age ** 2)
